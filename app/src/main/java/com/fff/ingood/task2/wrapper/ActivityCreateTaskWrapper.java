f3f0d3ada329ec99225965bef12bb7de6e933c01
Yoie_Huang <Yoie_Huang@asus.com>
2018-06-11 17:18:30 +0800

    [Task] restruct ActivityCreateTask

---
+++ app/src/main/java/com/fff/ingood/task2/wrapper/ActivityCreateTaskWrapper.java
@@ -0,0 +1,66 @@
+package com.fff.ingood.task2.wrapper;
+
+import com.fff.ingood.data.IgActivity;
+import com.fff.ingood.data.Person;
+import com.fff.ingood.task2.ActivityCreateTask;
+import com.fff.ingood.task2.AsyncResponder;
+import com.fff.ingood.task2.PersonLoginTask;
+import com.fff.ingood.tools.ParserUtils;
+import com.fff.ingood.tools.StringTool;
+
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_NWK_FAIL_INT;
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_PARSING_ERROR;
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_SUCCESS;
+import static com.fff.ingood.global.ServerResponse.TAG_SERVER_RESPONSE_STATUS_CODE;
+
+public class ActivityCreateTaskWrapper {
+    public interface ActivityCreateTaskWrapperCallback {
+        void onCreateActivitySuccess(String strIds);
+        void onCreateActivityFailure(Integer iStatusCode);
+    }
+
+    private ActivityCreateTask task;
+    private ActivityCreateTaskWrapper.ActivityCreateTaskWrapperCallback mCb;
+
+    public ActivityCreateTaskWrapper(ActivityCreateTaskWrapper.ActivityCreateTaskWrapperCallback cb) {
+        mCb = cb;
+        task = new ActivityCreateTask(new AsyncResponder<Integer, String>() {
+            @Override
+            public boolean makeOutput(String strJsonResponse) {
+                if(!StringTool.checkStringNotNull(strJsonResponse)) {
+                    setStatus(STATUS_CODE_NWK_FAIL_INT);
+                    return false;
+                }
+
+                String strStatusCode = ParserUtils.getStringByTag(TAG_SERVER_RESPONSE_STATUS_CODE, strJsonResponse);
+
+                if(StringTool.checkStringNotNull(strStatusCode)) {
+                    if (strStatusCode.equals(STATUS_CODE_SUCCESS)) {
+                        setStatus(Integer.parseInt(strStatusCode));
+                        setData(ParserUtils.getActivitiesByJson(strJsonResponse).get(0).getId());
+                        return true;
+                    } else {
+                        setStatus(Integer.parseInt(strStatusCode));
+                    }
+                } else {
+                    setStatus(STATUS_CODE_PARSING_ERROR);
+                }
+                return false;
+            }
+
+            @Override
+            public void onSuccess(String strIds) {
+                mCb.onCreateActivitySuccess(strIds);
+            }
+
+            @Override
+            public void onFailure(Integer iStatusCode) {
+                mCb.onCreateActivityFailure(iStatusCode);
+            }
+        });
+    }
+
+    public void execute(IgActivity activity) {
+        task.execute(activity);
+    }
+}
