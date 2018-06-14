b7bd1e1cceb8bcb1eb96c84f7b3b8136b31c3c93
elminsterii2 <elminsterii2@gmail.com>
2018-06-14 23:12:56 +0800

    [Ingood] 1. integrate animation into MainActivity.

--- app/src/main/java/com/fff/ingood/activity/MainActivity.java
+++ app/src/main/java/com/fff/ingood/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fff.ingood.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.fff.ingood.R;
@@ -17,8 +19,8 @@ import static com.fff.ingood.global.ServerResponse.getServerResponseDescriptions
 
 public class MainActivity extends AppCompatActivity implements Flow.FlowLogicCaller {
 
-    Activity mActivity;
-    CircleProgressBarDialog mWaitingDialog;
+    private MainActivity mActivity;
+    private CircleProgressBarDialog mWaitingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +37,7 @@ public class MainActivity extends AppCompatActivity implements Flow.FlowLogicCal
     @Override
     protected void onResume() {
         super.onResume();
-        Intent intent = new Intent(MainActivity.this, StartActivity.class);
-        startActivity(intent);
-
-        //mWaitingDialog.show(getSupportFragmentManager(), MainActivity.class.getName());
-        //FlowManager.getInstance().goLoginFlow(this);
+        startStartupAnimation();
     }
 
     @Override
@@ -59,5 +57,34 @@ public class MainActivity extends AppCompatActivity implements Flow.FlowLogicCal
         } else {
             Toast.makeText(mActivity, getServerResponseDescriptions().get(iStatusCode), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void startStartupAnimation() {
+        ImageView imgStartupAnimation = findViewById(R.id.imgStartupAnimation);
+        imgStartupAnimation.setImageResource(R.drawable.landingpage_bg);
+
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1.0f, 1.4f, 1.0f
+                , Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(3000);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mWaitingDialog.show(getSupportFragmentManager(), MainActivity.class.getName());
+                FlowManager.getInstance().goLoginFlow(mActivity);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imgStartupAnimation.startAnimation(scaleAnimation);
     }
 }
