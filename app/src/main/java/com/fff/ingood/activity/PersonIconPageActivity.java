a7f0c0727dade6a9b593a3071a9881e80d8f24b8
Chris <doofu1939@gmail.com>
2018-06-22 17:02:47 +0800

    Finish Uploading Person Image

--- app/src/main/java/com/fff/ingood/activity/PersonIconPageActivity.java
+++ app/src/main/java/com/fff/ingood/activity/PersonIconPageActivity.java
@@ -1,33 +1,48 @@
 package com.fff.ingood.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.*;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.fff.ingood.R;
 import com.fff.ingood.data.Person;
-import com.fff.ingood.flow.FlowManager;
-import com.fff.ingood.flow.PreferenceManager;
-import com.fff.ingood.flow.RegisterFlowLogic;
-import com.fff.ingood.task.AsyncResponder;
-import com.fff.ingood.task.DoPersonLogInTask;
-import com.fff.ingood.task.DoPersonVerifyTask;
-import com.fff.ingood.tools.ParserUtils;
+import com.fff.ingood.task.*;
 
-import static com.fff.ingood.activity.RegisterPrimaryPageActivity.API_RESPONSE_TAG;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.android.volley.Response.*;
 
 public class PersonIconPageActivity extends BaseActivity {
 
-    private Button mButton_Next;
-    private Button mButton_Send;
-    private EditText mEditText_VerifyCode;
+    private Button mButton_upLoad;
+    private Button mButton_get;
     private Person mUser = new Person();
-
-    private String mVerifyCode;
+    private ImageView ivShow;
+    private Bitmap photoBmp;
+    private EditText mEditText_Account;
+    private String url ;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,65 +58,92 @@ public class PersonIconPageActivity extends BaseActivity {
     @Override
     protected void initView(){
         super.initView();
-        mButton_Next = findViewById(R.id.btn_next);
-        mButton_Send = findViewById(R.id.btn_send);
-        mEditText_VerifyCode = findViewById(R.id.edit_verify);
-
+        mEditText_Account = findViewById(R.id.edit_account);
+        mButton_upLoad = findViewById(R.id.button_upload);
+        mButton_get = findViewById(R.id.btn_get);
+        ivShow = findViewById(R.id.showimg);
     }
 
     @Override
     protected void initData(){
         super.initData();
         mUser = (Person)getIntent().getSerializableExtra("user");
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 0x1111 && data != null) {
+            uri = data.getData();
+            try {
+                photoBmp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ivShow.setImageBitmap(photoBmp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImg(String account){
+
+        url = HttpProxy.HTTP_POST_API_PERSON_ACCESS_ICON+"/"+account+"/icon01";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String Response = jsonObject.getString(response);
+                        Log.d("response", Response);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                }, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", "icon01");
+                params.put("image", imageTobstring(photoBmp));
+                return params;
+            }
+        };
+        MySingleton.getInstance(PersonIconPageActivity.this).addToRequestQueue(stringRequest);
+    }
+
+    private String imageTobstring(Bitmap bmp){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+        byte[] imagebyte = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imagebyte, Base64.DEFAULT);
     }
 
     @Override
     protected void initListener(){
         super.initListener();
-        mButton_Next.setOnClickListener(new Button.OnClickListener() {
+        mButton_upLoad.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!mVerifyCode.isEmpty()
-                        && mVerifyCode.equals(mEditText_VerifyCode.getText().toString())){
-                    mUser.setVerifyCode(mVerifyCode);
-                    Class clsFlow = FlowManager.getInstance().goRegisterFlow();
-                    Intent intent = new Intent(mActivity, clsFlow);
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("user", mUser);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                }
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0x1111);
+                String email = mEditText_Account.getText().toString();
+                uploadImg(email);
             }
         });
 
-        mButton_Send.setOnClickListener(new Button.OnClickListener() {
+        mButton_get.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
-                Person userForVerify = new Person();
-                userForVerify.setEmail(mUser.getEmail());
-                DoPersonVerifyTask task = new DoPersonVerifyTask(mActivity,
-                        new AsyncResponder<String>() {
-                            @Override
-                            public void onSuccess(String strResponse) {
-                                if (ParserUtils.getStringByTag(API_RESPONSE_TAG,strResponse).contains("0")) {
-                                    Toast.makeText(PersonIconPageActivity.this, "doVerify OK", Toast.LENGTH_SHORT).show();
-                                    Person temp = ParserUtils.getPersonAttr(strResponse);
-                                    mVerifyCode = temp.getVerifyCode();
-
-
-
-                                }
-                            }
-                        });
-                task.execute(userForVerify);
 
             }
         });
     }
-
 
 }
