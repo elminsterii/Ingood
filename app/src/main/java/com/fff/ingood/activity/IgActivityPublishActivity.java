4cf288ad70e07a78a17797cf31418a23829e7826
elminsterii2 <elminsterii2@gmail.com>
2018-07-10 15:12:34 +0800

    [Ingood] 1. add layout for IgActivityPublish

---
+++ app/src/main/java/com/fff/ingood/activity/IgActivityPublishActivity.java
@@ -0,0 +1,39 @@
+package com.fff.ingood.activity;
+
+import android.os.Bundle;
+
+import com.fff.ingood.R;
+
+public class IgActivityPublishActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ig_publish);
+    }
+
+    @Override
+    protected void preInit() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initSystemUI() {
+
+    }
+}
