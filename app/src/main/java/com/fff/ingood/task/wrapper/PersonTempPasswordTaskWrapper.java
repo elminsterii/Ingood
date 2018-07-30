ba61940e1fe82720ae2887bde72637e12750b322
Chris <doofu1939@gmail.com>
2018-07-30 23:18:52 +0800

    Finished implementing PersonTempPasswordTask and its wrapper.

---
+++ app/src/main/java/com/fff/ingood/task/wrapper/PersonTempPasswordTaskWrapper.java
@@ -0,0 +1,69 @@
+package com.fff.ingood.task.wrapper;
+
+import com.fff.ingood.data.Person;
+import com.fff.ingood.task.AsyncHttpRequestResponder;
+import com.fff.ingood.task.PersonCheckExistTask;
+import com.fff.ingood.task.PersonTempPasswordTask;
+import com.fff.ingood.tools.ParserUtils;
+import com.fff.ingood.tools.StringTool;
+
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_NWK_FAIL_INT;
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_PARSING_ERROR;
+import static com.fff.ingood.global.ServerResponse.STATUS_CODE_SUCCESS;
+import static com.fff.ingood.global.ServerResponse.TAG_SERVER_RESPONSE_STATUS_CODE;
+
+/**
+ * Created by chris on 2018/7/30.
+ */
+
+public class PersonTempPasswordTaskWrapper {
+
+    public interface PersonTempPasswordTaskWrapperCallback {
+        void onPersonSetTempPassword();
+        void onPersonSetTempPasswordFailure(Integer iStatusCode);
+    }
+
+    private PersonTempPasswordTask task;
+    private PersonTempPasswordTaskWrapperCallback mCb;
+
+    public PersonTempPasswordTaskWrapper(PersonTempPasswordTaskWrapperCallback cb) {
+        mCb = cb;
+        task = new PersonTempPasswordTask(new AsyncHttpRequestResponder<Integer, Void>() {
+            @Override
+            public boolean parseResponse(String strJsonResponse) {
+                if(!StringTool.checkStringNotNull(strJsonResponse)) {
+                    setStatus(STATUS_CODE_NWK_FAIL_INT);
+                    return false;
+                }
+
+                String strStatusCode = ParserUtils.getStringByTag(TAG_SERVER_RESPONSE_STATUS_CODE, strJsonResponse);
+
+                if(StringTool.checkStringNotNull(strStatusCode)) {
+                    if (strStatusCode.equals(STATUS_CODE_SUCCESS)) {
+                        setStatus(Integer.parseInt(strStatusCode));
+                        return true;
+                    } else {
+                        setStatus(Integer.parseInt(strStatusCode));
+                    }
+                } else {
+                    setStatus(STATUS_CODE_PARSING_ERROR);
+                }
+                return false;
+            }
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                mCb.onPersonSetTempPassword();
+            }
+
+            @Override
+            public void onFailure(Integer iStatusCode) {
+                mCb.onPersonSetTempPasswordFailure(iStatusCode);
+            }
+        });
+    }
+
+    public void execute(Person person) {
+        task.execute(person);
+    }
+}
