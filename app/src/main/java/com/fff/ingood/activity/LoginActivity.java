3fa927a9a75492c7daff512536066d86fe2ac00c
Yoie_Huang <Yoie_Huang@asus.com>
2018-07-31 17:01:31 +0800

    [FBLogin] add dependency and login logic

--- app/src/main/java/com/fff/ingood/activity/LoginActivity.java
+++ app/src/main/java/com/fff/ingood/activity/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.fff.ingood.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginBehavior;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.fff.ingood.R;
 import com.fff.ingood.flow.Flow;
 import com.fff.ingood.flow.FlowManager;
 import com.fff.ingood.global.ServerResponse;
 import com.fff.ingood.global.SystemUIManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.fff.ingood.global.ServerResponse.getServerResponseDescriptions;
 
@@ -23,7 +43,13 @@ public class LoginActivity extends BaseActivity {
     private Button mButton_SignIn;
     private Button mButton_Register;
 
+    private ImageButton mImageButton_FBLogin;
+
     private LoginActivity mActivity;
+
+    // FB
+    private LoginManager mLoginManager;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +75,16 @@ public class LoginActivity extends BaseActivity {
     protected void initView(){
         mButton_SignIn = findViewById(R.id.btn_signin);
         mButton_Register = findViewById(R.id.btn_register);
+        mImageButton_FBLogin = findViewById(R.id.btn_facebook);
     }
 
     @Override
     protected void initData(){
-
-    }
+        // init facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // init LoginManager & CallbackManager
+        mLoginManager = LoginManager.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();    }
 
     @Override
     protected void initListener(){
@@ -69,6 +99,13 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 FlowManager.getInstance().goRegistrationFlow(mActivity);
+            }
+        });
+
+        mImageButton_FBLogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginFB();
             }
         });
     }
@@ -93,5 +130,80 @@ public class LoginActivity extends BaseActivity {
         } else {
             Toast.makeText(mActivity, getServerResponseDescriptions().get(iStatusCode), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void LoginFB() {
+        // 設定FB login的顯示方式 ; 預設是：NATIVE_WITH_FALLBACK
+        /**
+         * 1. NATIVE_WITH_FALLBACK
+         * 2. NATIVE_ONLY
+         * 3. KATANA_ONLY
+         * 4. WEB_ONLY
+         * 5. WEB_VIEW_ONLY
+         * 6. DEVICE_AUTH
+         */
+        mLoginManager.setLoginBehavior(LoginBehavior.NATIVE_WITH_FALLBACK);
+        // 設定要跟用戶取得的權限，以下3個是基本可以取得，不需要經過FB的審核
+        List<String> permissions = new ArrayList<>();
+        permissions.add("public_profile");
+        permissions.add("email");
+        permissions.add("user_friends");
+                                // 設定要讀取的權限
+                                mLoginManager.logInWithReadPermissions(this, permissions);
+        mLoginManager.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            if (response.getConnection().getResponseCode() == 200) {
+                                long id = object.getLong("id");
+                                String name = object.getString("name");
+                                String email = object.getString("email");
+                                Log.d("yoie", "Facebook id:" + id);
+                                Log.d("yoie", "Facebook name:" + name);
+                                Log.d("yoie", "Facebook email:" + email);
+                                // 此時如果登入成功，就可以順便取得用戶大頭照
+                                Profile profile = Profile.getCurrentProfile();
+                                // 設定大頭照大小
+                                Uri userPhoto = profile.getProfilePictureUri(300, 300);
+
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                // https://developers.facebook.com/docs/android/graph?locale=zh_TW
+                // 如果要取得email，需透過添加參數的方式來獲取(如下)
+                // 不添加只能取得id & name
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                // 用戶取消
+                Log.d("yoie", "Facebook onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // 登入失敗
+                Log.d("yoie", "Facebook onError:" + error.toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
